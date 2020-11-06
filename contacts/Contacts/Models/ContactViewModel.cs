6173d97fb5e5897c4c902049c11e4a2b0ab4e1a5
Filipe Lins <filipeallins@gmail.com>
2020-11-06 13:58:20 -0300

    Ajuste da classe ContactModel para ContactViewModel

---
+++ contacts/Contacts/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.Models
+{
+    public class ContactViewModel
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Nascimento
+        /// </summary>
+        [Required]
+        public DateTime Nascimento { get; set; }
+
+        /// <summary>
+        /// Sexo
+        /// </summary>
+        [Required]
+        public string Sexo { get; set; }
+    }
+}
