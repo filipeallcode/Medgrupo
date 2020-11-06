286bce65231f1d1a50e3d069724689425ea0cf0a
Filipe Lins <filipeallins@gmail.com>
2020-11-06 11:04:55 -0300

    Criação da solução, projeto principal, projeto de testes com referência ao projeto principal e classe ContactModel

---
+++ contacts/Contacts/Models/ContactModel.cs
@@ -0,0 +1,9 @@
+namespace Contacts.Models
+{
+    public class ContactModel
+    {
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string Sexo { get; set; }
+    }
+}
